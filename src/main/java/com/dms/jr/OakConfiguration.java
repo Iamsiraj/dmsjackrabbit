76abe158fd8c1b2942ee11634d77306b11b3f7e3
Mamoon Mehmood <mmehmood@iconsult.com.pk>
2023-10-20 15:59:24 +0500

    Changed datasource to file source

--- src/main/java/com/dms/jr/OakConfiguration.java
+++ src/main/java/com/dms/jr/OakConfiguration.java
@@ -1,48 +1,50 @@
 package com.dms.jr;
 
-
-import lombok.extern.slf4j.Slf4j;
 import org.apache.jackrabbit.api.JackrabbitRepository;
 import org.apache.jackrabbit.oak.Oak;
 import org.apache.jackrabbit.oak.jcr.Jcr;
-import org.apache.jackrabbit.oak.plugins.document.DocumentNodeStore;
-import org.apache.jackrabbit.oak.plugins.document.rdb.RDBDocumentNodeStoreBuilder;
+import org.apache.jackrabbit.oak.segment.SegmentNodeStore;
+import org.apache.jackrabbit.oak.segment.SegmentNodeStoreBuilders;
+import org.apache.jackrabbit.oak.segment.file.FileStore;
+import org.apache.jackrabbit.oak.segment.file.FileStoreBuilder;
+import org.apache.jackrabbit.oak.segment.file.InvalidFileStoreVersionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PreDestroy;
 import javax.jcr.Repository;
-import javax.sql.DataSource;
+import java.io.File;
+import java.io.IOException;
 
 @Configuration
-@Slf4j
 public class OakConfiguration {
 
+    private static Logger log = LoggerFactory.getLogger(OakConfiguration.class);
 
     private volatile Repository repository;
-
-    private final DataSource dataSource;
-
-    public OakConfiguration(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
 
 
     @Bean
     public Repository repository() {
+        try {
+            final FileStore fs = FileStoreBuilder.fileStoreBuilder(new File("repository")).build();
+            final SegmentNodeStore ns = SegmentNodeStoreBuilders.builder(fs).build();
+            repository = new Jcr(new Oak(ns)).createRepository();
+            log.info("Reository creaetd with nodeStore, {}", ns);
+        } catch (InvalidFileStoreVersionException e) {
+            log.error("Failed to create repository due to invalid file store version.", e);
+        } catch (IOException e) {
+            log.error("Failed to create repository due to IO exception.", e);
+        }
 
-        DocumentNodeStore store = RDBDocumentNodeStoreBuilder.newRDBDocumentNodeStoreBuilder()
-                .setRDBConnection(dataSource).build();
-
-        repository = new Jcr(new Oak(store)).createRepository();
-        log.info("Initializing OAK Repository ....");
         return repository;
     }
 
     @PreDestroy
     public void destroy() {
         if (repository instanceof JackrabbitRepository) {
-            log.info("Shutting Down OAK Repository ....");
             ((JackrabbitRepository) repository).shutdown();
         }
     }
